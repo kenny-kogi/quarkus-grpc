02052ad79b3dc4437a37e8b3b3b6da3579e1d54c
Kenneth Kogi <mwangiken497@gmail.com>
2022-11-22 17:23:13 +0300

    refr: setup grpc client

--- src/main/kotlin/controller/HelloWorldResource.kt
+++ src/main/kotlin/controller/HelloWorldResource.kt
@@ -1,8 +1,11 @@
 package controller
 
+import io.quarkus.example.Greeter
 import io.quarkus.example.GreeterGrpc
 import io.quarkus.example.HelloRequest
+import io.quarkus.grpc.GrpcClient
 import io.quarkus.grpc.GrpcService
+import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -10,12 +13,12 @@ import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-
+@ApplicationScoped
 @Path("/hello-resteasy")
 class HelloWorldResource {
     @Inject
-    @GrpcService()
-    var client: GreeterGrpc.GreeterBlockingStub? = null
+    @GrpcService("hello")
+    lateinit var client: GreeterGrpc.GreeterBlockingStub
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -27,6 +30,6 @@ class HelloWorldResource {
     @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
     fun hello(@PathParam("name") name: String?): String? {
-        return client?.sayHello(HelloRequest.newBuilder().setName(name).build())?.getMessage()
+        return client?.sayHello(HelloRequest.newBuilder().setName(name).build()).toString()
     }
 }
--- src/main/kotlin/service/HelloWorldService.kt
+++ src/main/kotlin/service/HelloWorldService.kt
@@ -6,10 +6,10 @@ import io.quarkus.example.HelloReply
 import io.quarkus.example.HelloRequest
 import javax.inject.Singleton
 
-
 @Singleton
 class HelloWorldService : GreeterGrpc.GreeterImplBase() {
-    override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
+
+    override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloReply?>) {
         val name = request.name
         val message = "Hello $name"
         responseObserver.onNext(HelloReply.newBuilder().setMessage(message).build())
